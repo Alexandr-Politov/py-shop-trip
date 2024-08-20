5c93ff7398a3a534779e4c08448f63bdad3830b7
Alexandr Politov <apolitov83@gmail.com>
2024-08-20 16:36:33 +0300

    Update customer.py  using Decimal

--- app/customer.py
+++ app/customer.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from decimal import Decimal
 
 
 @dataclass
@@ -6,5 +7,8 @@ class Customer:
     name: str
     product_cart: dict
     location: list[int]
-    money: int
+    money: Decimal
     car: dict
+
+    def __post_init__(self) -> None:
+        self.money = Decimal(str(self.money))

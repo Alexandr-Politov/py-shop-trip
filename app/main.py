b2cdc504ac9425b03905bbeb83905e60fa51d00c
Alexandr Politov <apolitov83@gmail.com>
2024-08-20 16:37:18 +0300

    Update main.py using Decimal

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+from decimal import Decimal, ROUND_HALF_UP
 import json
 
 from app.customer import Customer
@@ -7,7 +8,7 @@ from app.shop import Shop
 with open("app/config.json", "r") as json_file:
     data_dict = json.load(json_file)
 
-FUEL_PRICE = data_dict["FUEL_PRICE"]
+FUEL_PRICE = Decimal(data_dict["FUEL_PRICE"])
 
 customers = [Customer(*data_dict["customers"][i].values())
              for i in range(len(data_dict["customers"]))]
@@ -20,30 +21,30 @@ def shop_trip() -> None:
     for customer in customers:
         print(f"{customer.name} has {customer.money} dollars")
 
-        min_milk_cost = 0
-        min_bread_cost = 0
-        min_butter_cost = 0
-        min_total_cost = float("inf")
+        min_milk_cost = Decimal("0")
+        min_bread_cost = Decimal("0")
+        min_butter_cost = Decimal("0")
+        min_total_cost = Decimal("Infinity")
         min_shop = None
 
         for shop in shops:
-            distance = (((shop.location[0] - customer.location[0]) ** 2
-                        + (shop.location[1] - customer.location[1]) ** 2)
-                        ** 0.5)
-            fuel_cost = round((distance * customer.car["fuel_consumption"]
-                               / 100 * FUEL_PRICE) * 2, 2)
-            milk_cost = (customer.product_cart["milk"]
-                         * shop.products["milk"])
-            bread_cost = (customer.product_cart["bread"]
-                          * shop.products["bread"])
-            butter_cost = (customer.product_cart["butter"]
-                           * shop.products["butter"])
-            total_cost = round(
-                fuel_cost + milk_cost + bread_cost + butter_cost, 2
-            )
+            distance = Decimal((((shop.location[0] - customer.location[0]) ** 2
+                                 + (shop.location[1]
+                                    - customer.location[1]) ** 2) ** 0.5))
+            fuel_cost = (distance * Decimal(customer.car["fuel_consumption"])
+                         / Decimal("100") * FUEL_PRICE) * 2
+            milk_cost = (Decimal(str(customer.product_cart["milk"]))
+                         * Decimal(str(shop.products["milk"])))
+            bread_cost = (Decimal(str(customer.product_cart["bread"]))
+                          * Decimal(str(shop.products["bread"])))
+            butter_cost = (Decimal(str(customer.product_cart["butter"]))
+                           * Decimal(str(shop.products["butter"])))
+            total_cost = fuel_cost + milk_cost + bread_cost + butter_cost
+            total_cost = total_cost.quantize(Decimal("0.01"),
+                                             rounding=ROUND_HALF_UP)
 
-            print(f"{customer.name}'s trip to the "
-                  f"{shop.name} costs {total_cost}")
+            print(f"{customer.name}'s trip to "
+                  f"the {shop.name} costs {total_cost}")
 
             if total_cost <= min_total_cost:
                 min_total_cost = total_cost

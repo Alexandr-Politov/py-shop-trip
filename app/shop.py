a49b6422fef540a9ac87d568a15864364cca71e5
Alexandr Politov <apolitov83@gmail.com>
2024-09-02 18:34:42 +0300

    Update shop.py

--- app/shop.py
+++ app/shop.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from decimal import Decimal
 
 
 @dataclass
@@ -6,3 +7,17 @@ class Shop:
     name: str
     location: list[int]
     products: dict
+
+    def product_cost(self, product_name: str, quantity: int) -> Decimal:
+        product_cost = (Decimal(str(self.products[product_name]))
+                        * Decimal(quantity))
+        if product_cost % 1 == 0:
+            return product_cost.quantize(Decimal("0"))
+        return product_cost
+
+    def calc_total_cost(self, product_cart: dict) -> Decimal:
+        total_cost = Decimal("0")
+        for product in product_cart:
+            total_cost += (Decimal(str(self.products[product]))
+                           * Decimal(str(product_cart[product])))
+        return total_cost

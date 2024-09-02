4268015032eb3d0af1ffd2bab9419e399bc0a6d2
Alexandr Politov <apolitov83@gmail.com>
2024-09-02 18:34:22 +0300

    Update customer.py

--- app/customer.py
+++ app/customer.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
 from decimal import Decimal
 
+from app.shop import Shop
+
 
 @dataclass
 class Customer:
@@ -12,3 +14,11 @@ class Customer:
 
     def __post_init__(self) -> None:
         self.money = Decimal(str(self.money))
+
+    def calc_dist(self, shop: Shop) -> Decimal:
+        return Decimal((((shop.location[0] - self.location[0]) ** 2
+                         + (shop.location[1] - self.location[1]) ** 2) ** 0.5))
+
+    def calc_trip_cost(self, shop: Shop, fuel_price: Decimal) -> Decimal:
+        return (self.calc_dist(shop) * Decimal(self.car["fuel_consumption"])
+                / Decimal("100") * fuel_price)
